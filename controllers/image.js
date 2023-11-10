fb0c3114505cfc134bfb8530c063fc6077e235e0
MAR-Rehman <murad_rehman@hotmail.com>
2023-11-10 22:23:34 +0000

    adding api files

---
+++ controllers/image.js
@@ -0,0 +1,35 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: '7d3b0658231544d6b75322bc23e550a0'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict('face-detection', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+    // entries[0] --> this used to return the entries
+    // TO
+    // entries[0].entries --> this now returns the entries
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall
+};
