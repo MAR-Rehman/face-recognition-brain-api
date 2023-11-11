e4b864217e47a25068449e1ecee10e2f8d93357d
MAR-Rehman <murad_rehman@hotmail.com>
2023-11-11 18:02:03 +0000

    changed / on server.js to its working

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors())
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 // Test only - when you have a database variable you want to use
-app.get('/', (req, res)=> { res.send(database.users) })
+app.get('/', (req, res)=> {('its working') })
 app.post('/signin', signin.handleSignin(db, bcrypt)) 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
